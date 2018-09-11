7f687804e4c2bb4e0a71833e044cb8f6ab619322
mccrush <mccrush2027@gmail.com>
2018-09-11 17:49:18 +0700

    Добавляет компонент Header

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <template>
   <div id="app">
+    <HeaderMy msgmy="Crush 2"/>
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
+import HeaderMy from './components/HeaderMy.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    HeaderMy
   }
-}
+};
 </script>
 
 <style>
@@ -23,6 +26,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/HeaderMy.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="nav-scroller bg-white shadow-sm ">
+      <nav class="nav justify-content-end">
+        <a class="nav-link" href="#">Почта</a>
+        <a class="nav-link" href="#">Картинки</a>
+        <form class="form-inline">
+          <button class="btn btn-sm mr-sm-3 ml-sm-1 btn-light" type="button"></button>
+        </form>   
+      </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HeaderMy',
+  props: {
+    msgmy: String
+  }
+};
+</script>
+
+<style scoped>
+a.nav-link {
+  font-size: 14px;
+  color: #6c757d;
+  padding: 0.5rem 0.5rem;
+}
+
+a.nav-link:hover {
+  font-size: 14px;
+  color: #343a40;
+}
+
+button.btn-light {
+  width: 26px;
+  height: 26px;
+  background: url('/assets/ic_apps_black_24dp_1x.png') center center no-repeat;
+  opacity: 0.5;
+}
+
+button.btn-light:hover {
+  opacity: 0.9;
+}
+</style>
