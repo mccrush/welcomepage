27d510368347f58afc1451862bb333826273d7db
mccrush <mccrush2027@gmail.com>
2018-09-12 13:50:39 +0700

    Добавляет и натсраивает центральные блоки и Лого

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,23 @@
 <template>
-  <div id="app">
-    <Header msg="Crush 1"/>
-    <Launcher msg="Crush 2"/>
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="cover-container d-flex flex-column h-100">
+    <Header/>
+    <Launcher/>
+    <div class="mb-auto"></div> 
+    <div class="border d-flex flex-column">
+      <Logo msg="Google 1" class="col-12 align-self-center"/>
+      <Logo msg="Google 2" class="col-6 align-self-center"/>
+      <Logo msg="Google 3" class="col-8 align-self-center"/>
+    </div>
+    <div class="mt-auto"></div>
+    <!-- <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
 <script>
 import Header from './components/Header.vue';
 import Launcher from './components/Launcher.vue';
+import Logo from './components/Logo.vue';
 import HelloWorld from './components/HelloWorld.vue';
 
 export default {
@@ -17,6 +25,7 @@ export default {
   components: {
     Header,
     Launcher,
+    Logo,
     HelloWorld
   }
 };
@@ -29,5 +38,17 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+
+.h-100 {
+  height: 100% !important;
+}
+
+.mb-auto {
+  margin-bottom: auto !important;
+}
+
+.mt-auto {
+  margin-top: auto !important;
 }
 </style>
---
+++ src/components/Logo.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="logo" class="shadow-sm bg-white"> <!--  shadow-sm -->
+      <img alt="Google" height="92" src="assets/googlelogo_color_272x92dp.png" width="272">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Logo',
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<style scoped>
+div#logo {
+  margin: 10px;
+}
+</style>
