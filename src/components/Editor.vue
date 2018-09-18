2de532d5f61d58aafe4535d902fc43d4b0118982
mccrush <mccrush2027@gmail.com>
2018-09-18 11:27:48 +0700

    Реализует первичный метод сохранения данных

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -2,11 +2,11 @@
   <div class="container-fluid collapse position-absolute bg-white shadow-lg" id="collapseEditor">
       <div class="row justify-content-center"><!-- justify-content-center -->
         <div class="col-6 text-left"> <!-- align-self-center  bg-info  is-invalid  is-valid-->  
-              <input type="text" class="form-control form-control-sm mt-3" id="inpitLink" placeholder="Insert Link"  autofocus :value=this.href>
+              <input type="text" class="form-control form-control-sm mt-3" id="inpitLink" placeholder="Insert Link"  autofocus v-on:keyup.enter="closeEditor" v-model.trim="href" v-on:input="saveChange">
               <button type="button" class="close position-absolute bg-white t-10" aria-label="Close" title="Clear form">
                 <span aria-hidden="true" class="p-2" v-on:click.self="clearHref">&times;</span>
               </button>
-              <input type="text" class="form-control form-control-sm mt-3 mb-3" id="inpitTitle" placeholder="Insert Title" :value=this.title>
+              <input type="text" class="form-control form-control-sm mt-3 mb-3" id="inpitTitle" placeholder="Insert Title" v-on:keyup.enter="closeEditor" v-model.trim="title"  v-on:input="saveChange">
               <button type="button" class="close position-absolute bg-white t-30" aria-label="Close" title="Clear form">
                 <span aria-hidden="true" class="p-2" v-on:click.self="clearTitle">&times;</span>
               </button>
@@ -16,7 +16,9 @@
 </template>
 
 <script>
+// :value=this.title
 import { eventEmitter } from '../main.js';
+import { saveHref, saveTitle } from '../core/datasave.js';
 
 export default {
   name: 'Editor',
@@ -37,14 +39,26 @@ export default {
   },
   methods: {
     clearHref: function() {
-      document.getElementById('inpitLink').value = '';
-      document.getElementById('inpitLink').placeholder = '';
+      this.href = '';
+      //document.getElementById('inpitLink').value = '';
+      //document.getElementById('inpitLink').placeholder = '';
       document.getElementById('inpitLink').focus();
     },
     clearTitle: function() {
-      document.getElementById('inpitTitle').value = '';
-      document.getElementById('inpitTitle').placeholder = '';
+      this.title = '';
+      //document.getElementById('inpitTitle').value = '';
+      //document.getElementById('inpitTitle').placeholder = '';
       document.getElementById('inpitTitle').focus();
+    },
+    closeEditor: function() {
+      $('#collapseEditor').collapse('hide');
+      saveHref(this.href);
+      saveTitle(this.title);
+    },
+    saveChange: function() {
+      saveHref(this.href);
+      saveTitle(this.title);
+      //console.log(document.getElementById('inpitTitle').value);
     }
   }
 };
---
+++ src/core/datasave.js
@@ -0,0 +1,15 @@
+export function saveHref(data) {
+  var dt = dt + data;
+  var mydata = true;
+  //var data = JSON.parse(localStorage.getItem(STORAGE_KEY_G) || '[]');
+  console.log(data);
+  return mydata;
+}
+
+export function saveTitle(data) {
+  var dt = dt + data;
+  var mydata = true;
+  //var data = JSON.parse(localStorage.getItem(STORAGE_KEY_G) || '[]');
+  console.log(data);
+  return mydata;
+}
