a41436585dd9e3f69e2ad31574a1a086f59a8d5f
mccrush <mccrush2027@gmail.com>
2018-09-18 10:31:01 +0700

    Добавляет в Storage свойство position

--- src/components/storage.js
+++ src/components/storage.js
@@ -4,49 +4,57 @@ export function loadList() {
     title: "My Title 11",
     href: "http://google.com",
     icon: "https://s2.googleusercontent.com/s2/favicons?domain_url=https://trello.com/b/wNz9Hbop/01-tasklist&amp;alt=s&amp;sz=32",
-    thumb: "https://mini.s-shot.ru/1024x768/164/png/Z200/?mix.sibnet.ru"
+    thumb: "https://mini.s-shot.ru/1024x768/164/png/Z200/?mix.sibnet.ru",
+    position: 1
   }, {
     id: 2,
     title: "My Title 22",
     href: "#",
     icon: "https://s2.googleusercontent.com/s2/favicons?domain_url=https://trello.com/b/wNz9Hbop/01-tasklist&amp;alt=s&amp;sz=32",
-    thumb: "https://mini.s-shot.ru/1024x768/JPEG/154/Z200/?mccrush.ru"
+    thumb: "https://mini.s-shot.ru/1024x768/JPEG/154/Z200/?mccrush.ru",
+    position: 2
   }, {
     id: 3,
     title: "My Title 33",
     href: "http://google.com",
     icon: "https://s2.googleusercontent.com/s2/favicons?domain_url=https://trello.com/b/wNz9Hbop/01-tasklist&amp;alt=s&amp;sz=32",
-    thumb: "https://mini.s-shot.ru/1024x768/JPEG/1024/Z200/?vk.com"
+    thumb: "https://mini.s-shot.ru/1024x768/JPEG/1024/Z200/?vk.com",
+    position: 3
   }, {
     id: 4,
     title: "My Title 44",
     href: "http://google.com",
     icon: "https://s2.googleusercontent.com/s2/favicons?domain_url=https://trello.com/b/wNz9Hbop/01-tasklist&amp;alt=s&amp;sz=32",
-    thumb: "https://mini.s-shot.ru/1024x768/154/png/Z200/?mail.ru"
+    thumb: "https://mini.s-shot.ru/1024x768/154/png/Z200/?mail.ru",
+    position: 4
   }, {
     id: 5,
     title: "My Title 55 justify-content-end",
     href: "http://google.com",
     icon: "https://s2.googleusercontent.com/s2/favicons?domain_url=https://trello.com/b/wNz9Hbop/01-tasklist&amp;alt=s&amp;sz=32",
-    thumb: "https://mini.s-shot.ru/1024x768/164/png/Z200/?mix.sibnet.ru"
+    thumb: "https://mini.s-shot.ru/1024x768/164/png/Z200/?mix.sibnet.ru",
+    position: 5
   }, {
     id: 6,
     title: "My Title 66 justify-content-end",
     href: "#",
     icon: "https://s2.googleusercontent.com/s2/favicons?domain_url=https://trello.com/b/wNz9Hbop/01-tasklist&amp;alt=s&amp;sz=32",
-    thumb: "https://mini.s-shot.ru/1024x768/JPEG/154/Z200/?mccrush.ru"
+    thumb: "https://mini.s-shot.ru/1024x768/JPEG/154/Z200/?mccrush.ru",
+    position: 6
   }, {
     id: 7,
     title: "My Title 77 justify-content-end",
     href: "http://google.com",
     icon: "https://s2.googleusercontent.com/s2/favicons?domain_url=https://trello.com/b/wNz9Hbop/01-tasklist&amp;alt=s&amp;sz=32",
-    thumb: "https://mini.s-shot.ru/1024x768/JPEG/1024/Z200/?vk.com"
+    thumb: "https://mini.s-shot.ru/1024x768/JPEG/1024/Z200/?vk.com",
+    position: 7
   }, {
     id: 8,
     title: "My Title 88 justify-content-end",
     href: "http://google.com",
     icon: "https://s2.googleusercontent.com/s2/favicons?domain_url=https://trello.com/b/wNz9Hbop/01-tasklist&amp;alt=s&amp;sz=32",
-    thumb: "https://mini.s-shot.ru/1024x768/154/png/Z200/?mail.ru"
+    thumb: "https://mini.s-shot.ru/1024x768/154/png/Z200/?mail.ru",
+    position: 8
   }];
   //var data = JSON.parse(localStorage.getItem(STORAGE_KEY_G) || '[]');
   return mydata;
